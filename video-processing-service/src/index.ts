d9224494c0832297088ceff75b3aed28ea08aad3
Aakash <aakashit13@gmail.com>
2023-09-17 00:37:22 +0530

    local videoProcessing with FFmpeg

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -4,9 +4,29 @@ import ffmpeg from "fluent-ffmpeg";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
 
-app.get("/process-video", (req, res) => {
-  res.send("Hello World!");
+app.post("/processvideo", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath)
+    res.status(400).send({
+      status: "Failure",
+      message: "File path missing",
+    });
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      res
+        .status(200)
+        .send({ status: "Success", message: "Video processing started" });
+    })
+    .on("error", err => {
+      res.status(500).send({ status: "Failure", message: err.message });
+    })
+    .save(outputFilePath);
 });
 
 app.listen(PORT, () => {
